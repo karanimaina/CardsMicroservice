c31d9173e83fbff0b9011d3fcc8a7ce504c73016
felixmaina <karani.maina2010@gmail.com>
2022-10-25 23:41:41 +0300

    generate a cad class

--- src/main/java/com/example/cards/model/Card.java
+++ src/main/java/com/example/cards/model/Card.java
@@ -1,2 +1,25 @@
-package com.example.cards.model;public class Card {
+package com.example.cards.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private  Long customerId;
+    private String cardNo;
+    private String cardType;
+    private int totalLimit;
+    private int ammountUsed;
+    private int availableAmmount;
+    private Date createdDate;
 }
--- src/main/java/com/example/cards/repo/CardRepository.java
+++ src/main/java/com/example/cards/repo/CardRepository.java
@@ -1,6 +1,10 @@
 package com.example.cards.repo;
 
+import com.example.cards.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardRepository extends JpaRepository <Card,Long{
+import java.util.List;
+
+public interface CardRepository extends JpaRepository <Card,Long>{
+   List<Card>findCustomerById(long customerId)
 }
